21ecad999ae5f66e7ca8a8510ae61297573c1655
vivien-z <yuanyuanzhu000@gmail.com>
2021-12-28 15:56:54 -0500

    setup frontend with bootstrap and it's dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'page#home'
+  root to: 'pages#home'
 
   resources :users, only: [:show] do 
     resources :reservations, only: [:index]
