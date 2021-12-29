8d68ed0b1f23dcea639bce197c3ae1fb67cc656c
vivien-z <yuanyuanzhu000@gmail.com>
2021-12-29 15:55:14 -0500

    added flatpickr for reservation date range

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
 import 'bootstrap'
+import { initFlatpickr } from "../plugins/flatpickr";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+  initFlatpickr()
+});
