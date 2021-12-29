48c498aedd305bbe0f9d24cd38da0b82c197f67f
vivien-z <yuanyuanzhu000@gmail.com>
2021-12-29 14:33:40 -0500

    set pundit policy for reservation

---
+++ app/policies/reservation_policy.rb
@@ -0,0 +1,29 @@
+class ReservationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    is_owner?
+  end
+
+  def destroy?
+    is_owner?
+  end
+
+  private
+
+  def is_owner?
+    user && user == record.user
+  end
+end
