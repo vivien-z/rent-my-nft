3aa4c7b18aebcf21a36f91f779a47d520521803f
vivien-z <yuanyuanzhu000@gmail.com>
2022-01-04 11:47:30 -0500

    added PgSearch gem

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -2,7 +2,17 @@ class Nft < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
   has_one_attached :image
-  
+
+  # Search setting
+  default_scope {order("created_at DESC")}
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_synopsis,
+    against: [ :name, :synopsis ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  # Geocode address
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
